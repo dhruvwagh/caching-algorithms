e7c8608167013f9a2d05d2d954cf159b64342248
gabriel-domingues <gabrieldomingues.ba@gmail.com>
2022-08-24 11:12:22 -0300

    New: Preload arc traces

--- bench.cpp
+++ bench.cpp
@@ -10,32 +10,30 @@
 // See https://researcher.watson.ibm.com/researcher/view_person_subpage.php?id=4700 for
 // ARC traces
 
-using trace = std::pair<size_t, size_t>;
-
-std::vector<trace> loadmem(const char* fname) {
+std::vector<size_t> load_mem(const char* fname) {
   std::ifstream f(fname);
-  std::vector<trace> io;
-  io.reserve(1UL << 19);
+  std::vector<size_t> io;
+  io.reserve(1UL << 23);
   std::string dummy;
-  trace tmp = {0UL, 0UL};
-  while (((f >> tmp.first) &&
-          (f >> tmp.second) &&
-          (f >> dummy) &&
-          (f >> dummy)))
-    io.push_back(tmp);
+  for (size_t start, length = 0UL;
+       (f >> start) &&
+       (f >> length) &&
+       (f >> dummy) &&
+       (f >> dummy);) {
+    for (auto key = start; key < start + length; ++key)
+      io.push_back(key);
+  }
   return io;
 }
 
 template <class Cache>
-auto hit_rate(std::vector<trace> io, Cache cache) {
+auto hit_rate(std::vector<size_t> trace, Cache cache) {
   cache.describe();
-  size_t total = 0;
+  size_t total = trace.size();
   size_t hit = 0;
-  for (auto [start, length] : io) {
-    for (auto key = start; key < start + length; ++key)
-      hit += cache.set(key, nullptr);
-    total += length;
-  }
+  for (auto key : trace)
+    hit += cache.set(key, nullptr);
+
   auto ratio = (double)hit / (double)total;
   std::cout << hit << " / " << total << " = " << ratio << " = " << ratio * 100 << " %" << std::endl;
 }
@@ -44,7 +42,7 @@ int main(int argc, char const* argv[]) {
   if (argc < 2) return 1;
 
   auto fname = argv[1];
-  auto io = loadmem(fname);
+  auto io = load_mem(fname);
 
   const size_t size = 1 << 15;
 
